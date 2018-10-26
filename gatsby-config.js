bbac25bea0ce8574913950fbc566ff2e9212321e
shannonbux <32467162+shannonbux@users.noreply.github.com>
2018-10-26 15:51:20 -0600

    Added preview information
    Got this from Sam's document:
    
    `host: process.env.GATSBY_CONTENTFUL_HOST`

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,8 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: `g0b694a663ft`,
-        accessToken: `cc94019b03af3894ec87eded57fffd67c6785f1c6fb6b2c3e67ded27682b0c02`
+        accessToken: `cc94019b03af3894ec87eded57fffd67c6785f1c6fb6b2c3e67ded27682b0c02`,
+        host: process.env.GATSBY_CONTENTFUL_HOST
       }
     },
     `gatsby-transformer-sharp`,
