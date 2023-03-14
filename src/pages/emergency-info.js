72512b8a29c99e8af06922a0aea4093a1855c580
Shannon <32467162+shannonbux@users.noreply.github.com>
2023-03-14 10:47:00 -0700

    Update emergency-info.js

--- src/pages/emergency-info.js
+++ src/pages/emergency-info.js
@@ -2,11 +2,10 @@ import React from "react";
 
 export default ({ data }) => (
   <div>
-    <h1>Emergency Info</h1>
+    <h1>Veterinarian & Emergency Info</h1>
     <p>
       Watson can eat many human foods, like any plain, lean cooked meat, rice,
-      corn, berries, celery, cucumber, fish. The important things to avoid are
-      as follows:
+      corn, berries, celery, cucumber, fish. Call a pet emergency support line if he eats any of the following:
     </p>
     <ul>
       <li>
@@ -18,20 +17,8 @@ export default ({ data }) => (
       <li>Tree nuts</li>
       <li>Grapes</li>
     </ul>
-    <p>
-      If you know he ate something in the list above, you can squirt/pour one
-      tablespoon of hydrogen peroxide into his mouth so that he swallows it.
-      This will force him to throw up. Then, call someone from the emergency
-      contacts below to ask what to do next!
-    </p>
     <br />
     <h2>Emergency Contacts</h2>
-    <br />
-    <h4>Me</h4>
-    <p>
-      Call me first! My cell phone is 435-901-1640. I can receive calls and
-      texts there; calls are better for emergencies.{" "}
-    </p>
     <br />
     <h4>
       Campus Veterinary Clinic | 1807 Martin Luther King Jr Blvd | Berkeley, CA
