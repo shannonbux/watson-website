43fbf3461b2c0cdff9318a060ca03174ffd3095c
Shannon <32467162+shannonbux@users.noreply.github.com>
2023-03-14 10:45:07 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,7 @@ export default () => (
   <div>
     <h3>Welcome!</h3>
     <p>
-      If you have arrived at this site, it's probably because you are
-      babysitting Watson. By clicking on the links to different pages, you'll
+      Watson is our awesome dog! By clicking on the links to different pages, you'll
       find his daily schedule for sleep, walks, and eating, and a list of
       command phrases that he understands. You will also find a collection of
       photos that show Watson's professional capabilities :) Enjoy!
